89ae4ad165ccb4c5e3387d7763b9710294538b3b
Chris <890popbox@users.noreply.github.com>
2020-06-21 02:40:35 -0700

    update 6/21

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Auto-Retaliate
 
 - retal     : toggles it on and off
+- retal [whole number] : will set your retal delay to this number in ms.
 - retal on  : makes sure it is on
 - retal off : makes sure it is off
+
+(default settings: OFF, 0ms)
--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 module.exports = function retaliate(mod) {
-const {command} = mod;
-
-    let RETALIATE = {
-            reserved: 0,
-            npc: false,
-            type: 1,
-            huntingZoneId: 0,
-            id: 0
-        },
+	const {command} = mod,
         RETALIATE_IDs = [
             131000, // Warrior
             111000, // Lancer
@@ -21,65 +13,64 @@ const {command} = mod;
             201000, // Gunner
             121000, // Brawler
             101000, // Ninja
-            181099, // Valkyrie
+            181000 // Valkyrie
         ];
 
-    let w,
+    let RETALIATE = {
+            reserved: 0,
+            npc: false,
+            type: 1,
+            huntingZoneId: 0,
+            id: 0
+        },
+		delay = 0,
+		w,
         loc,
         dest,
         job,
         templateId,
-		enabled = true;
+		enabled = false;
 
 //retal toggles it.. on and off turns it on or off
 command.add('retal', (arg) => {
-        if (arg === undefined) {
-            enabled = !enabled;
-            command.message((enabled ? 'Enabled' : 'Disabled') + '.');
-        }
-		else if(arg.toLowerCase() === "off")
-        {
-            enabled = false;
-            command.message((enabled ? 'Enabled' : 'Disabled') + '.');
-        }
-        else if(arg.toLowerCase() === "on")
-        {
-            enabled = true;
-            command.message((enabled ? 'Enabled' : 'Disabled') + '.');
-        }
+    if (arg === undefined) enabled = !enabled;
+	else if (!isNaN(arg)) delay = parseInt(arg);
+    else if(arg.toLowerCase() === "off") enabled = false;
+    else if(arg.toLowerCase() === "on") enabled = true;
+    command.message((enabled ? 'Enabled.' : 'Disabled.'));
 });
 
-    mod.hook('S_LOGIN', 14, (event) => {
-        job = (event.templateId - 10101) % 100;
+//this figures out what class you are.. (class retal ids are different)
+    mod.game.on('enter_game', () =>  {
+		templateId = mod.game.me.templateId;
+        job = (templateId - 10101) % 100;
         RETALIATE.id = RETALIATE_IDs[job];
-        templateId = event.templateId;
     });
 
-    mod.hook('C_PLAYER_LOCATION', 5, (event) => {
-        w = event.w;
-        loc = event.loc;
-        dest = event.dest;
-    });
-
-    mod.hook('S_EACH_SKILL_RESULT', 14, (event) => {
-        if (event.reaction.skill.id !== (templateId * 100) + 2)
-            return;
-//toggle it on and off
-        if(enabled===true){
+//this is a function that will cast your retal skill for you, it passes in a w,loc
+    const retal = (w, loc) => {
         mod.send('C_START_SKILL', 7, {
-                skill: RETALIATE,
-                w: w,
-                loc: loc,
-                dest: dest,
-                unk: true,
-                moving: false,
-                cont: false,
-                target: 0,
-                unk2: false
+            skill: RETALIATE,
+            w: w,
+            loc: loc,
+            dest: {x:0,y:0,z:0},
+            unk: true,
+            moving: false,
+            cont: false,
+            target: 0,
+            unk2: false
             }
         );
-		}
-		
-		
+    }
+
+    mod.hook('S_EACH_SKILL_RESULT', 14, (event) => {
+
+//make sure that it is enabled or not.. and using right skill
+    if ((event.reaction.skill.id !== (templateId * 100) + 2) || !enabled)
+		return;
+
+//cast with or without delay
+	setTimeout(retal, delay, event.reaction.w, event.reaction.loc);
+
     });
 };
